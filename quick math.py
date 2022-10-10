7ee401916f073ae3326887821b60e092a177b169
ocean008 <personalocean008@gmail.com>
2022-10-10 22:12:22 +0700

    fix input
    change:
    multiply to subtract
    fix bug when input is string and it's error, to loop the program

--- quick math.py
+++ quick math.py
@@ -1,7 +1,11 @@
 #naming things
-#version 1.0
+#version 2.0
+'''
+change:
+multiply to subtract
+fix bug when input is string and it's error, to loop the program
+'''
 
-from multiprocessing.resource_sharer import stop
 import random
 
 def add(x, y):
@@ -18,47 +22,52 @@ def divide(x, y):#for future upgrade
 
 print("choose the game.")
 print("1.Add")
-print("2.multiply")
+print("2.subtract")
 
 #let's user choose
 choice = input("Enter choice(1/2): ")
 
 
 while True:
-    #for addition
-    if choice == "1":
-        #random integer from 1-10
-        a=random.randrange(1,10)
-        b=random.randrange(1,10)
-        print(a," + ", b)
-        question=a+b
-        answer=input("put your answer : ")
-        #to break
-        if answer == "break":
-            break
-        if question == int(answer):
-            print("\tcorrect")
-            print("")
-        else:
-            print("\twrong answer is :",question)
-            print("")
-
-    #for multiplication
-    if choice == "2":
-        #random integer from 1-10
-        a=random.randrange(1,10)
-        b=random.randrange(1,10)
-        print(a," x ", b)
-        question=a*b
-        answer=int(input("put your answer : "))
-        #to break
-        if answer == "break":
-            break
-        if question == answer:
-            print("\tcorrect")
-            print("")
-        else:
-            print("\twrong answer is :",question)
-            print("")
+    try:
+        #for addition
+        if choice == "1":
+            #random integer from 1-10
+            a=random.randrange(1,10)
+            b=random.randrange(1,10)
+            print(a," + ", b)
+            question=a+b
+            answer=input("put your answer : ")
+            #to break
+            if answer == "break":
+                break
+            if question == int(answer):
+                print("\tcorrect")
+                print("")
+            else:
+                print("\twrong answer is :",question)
+                print("")
+    except:
+        pass
+    try:
+        #for subtract
+        if choice == "2":
+            #random integer from 1-10
+            a=random.randrange(1,10)
+            b=random.randrange(1,10)
+            print(a," - ", b)
+            question=a-b
+            answer=int(input("put your answer : "))
+            #to break
+            if answer == "break":
+                break
+            if question == answer:
+                print("\tcorrect")
+                print("")
+            else:
+                print("\twrong answer is :",question)
+                print("")
+    except:
+        pass
 
 input=("thank for playing the game")
